19a51d5e1f060ecc31b7c310e7993778044c92ee
Raul Metsma <raul@metsma.ee>
2021-04-13 11:08:25 +0300

    Multiple card support
    WE2-377
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- src/autoSelectSupportedCard.cpp
+++ src/autoSelectSupportedCard.cpp
@@ -38,63 +38,54 @@ inline CardInfo::ptr connectToCard(const pcsc_cpp::Reader& reader)
 namespace electronic_id
 {
 
-CardInfo::ptr autoSelectSupportedCard()
+std::vector<CardInfo::ptr> availableSupportedCards()
 {
+    std::vector<pcsc_cpp::Reader> readers;
     try {
-        const auto readers = pcsc_cpp::listReaders();
+        std::vector<CardInfo::ptr> list;
+        readers = pcsc_cpp::listReaders();
 
         if (readers.empty()) {
             throw AutoSelectFailed(AutoSelectFailed::Reason::NO_READERS);
         }
-        if (readers.size() == 1) {
-            const auto& reader = readers[0];
-            if (!reader.isCardInserted()) {
-                throw AutoSelectFailed(AutoSelectFailed::Reason::SINGLE_READER_NO_CARD);
-            }
-            if (!isCardSupported(reader.cardAtr)) {
-                throw AutoSelectFailed(AutoSelectFailed::Reason::SINGLE_READER_UNSUPPORTED_CARD);
-            }
-            return connectToCard(reader);
-        }
-
-        // Multiple readers.
 
         auto seenCard = false;
-        auto seenSupportedCard = CardInfo::ptr();
-
         for (const auto& reader : readers) {
             if (!reader.isCardInserted()) {
                 continue;
             }
             seenCard = true;
             if (isCardSupported(reader.cardAtr)) {
-                if (seenSupportedCard) {
-                    throw AutoSelectFailed(AutoSelectFailed::Reason::MULTIPLE_SUPPORTED_CARDS);
-                }
-                seenSupportedCard = connectToCard(reader);
+                list.push_back(connectToCard(reader));
             }
         }
 
         if (!seenCard) {
-            throw AutoSelectFailed(AutoSelectFailed::Reason::MULTIPLE_READERS_NO_CARD);
+            throw AutoSelectFailed(readers.size() > 1
+                                       ? AutoSelectFailed::Reason::MULTIPLE_READERS_NO_CARD
+                                       : AutoSelectFailed::Reason::SINGLE_READER_NO_CARD);
         }
 
-        if (!seenSupportedCard) {
-            throw AutoSelectFailed(AutoSelectFailed::Reason::MULTIPLE_READERS_NO_SUPPORTED_CARD);
+        if (list.empty()) {
+            throw AutoSelectFailed(
+                readers.size() > 1 ? AutoSelectFailed::Reason::MULTIPLE_READERS_NO_SUPPORTED_CARD
+                                   : AutoSelectFailed::Reason::SINGLE_READER_UNSUPPORTED_CARD);
         }
 
-        return seenSupportedCard;
+        return list;
 
     } catch (const pcsc_cpp::ScardServiceNotRunningError&) {
         throw AutoSelectFailed(AutoSelectFailed::Reason::SERVICE_NOT_RUNNING);
     } catch (const pcsc_cpp::ScardNoReadersError&) {
         throw AutoSelectFailed(AutoSelectFailed::Reason::NO_READERS);
     } catch (const pcsc_cpp::ScardNoCardError&) {
-        // We don't know if SINGLE_READER_NO_CARD or MULTIPLE_READERS_NO_CARD, but
-        // SINGLE_READER_NO_CARD is more common.
-        throw AutoSelectFailed(AutoSelectFailed::Reason::SINGLE_READER_NO_CARD);
+        throw AutoSelectFailed(readers.size() > 1
+                                   ? AutoSelectFailed::Reason::MULTIPLE_READERS_NO_CARD
+                                   : AutoSelectFailed::Reason::SINGLE_READER_NO_CARD);
     } catch (const pcsc_cpp::ScardCardRemovedError&) {
-        throw AutoSelectFailed(AutoSelectFailed::Reason::SINGLE_READER_NO_CARD);
+        throw AutoSelectFailed(readers.size() > 1
+                                   ? AutoSelectFailed::Reason::MULTIPLE_READERS_NO_CARD
+                                   : AutoSelectFailed::Reason::SINGLE_READER_NO_CARD);
     } catch (const pcsc_cpp::ScardError&) {
         // FIXME: this must be removed, just a placeholder until error handling work lands in main
         throw AutoSelectFailed(AutoSelectFailed::Reason::SCARD_ERROR);
